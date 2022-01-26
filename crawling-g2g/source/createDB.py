2074d52d1c20e1f29432834eb333a7eed2081ada
hangack <hangack7@gmail.com>
2022-01-26 10:14:29 +0900

    project3

---
+++ crawling-g2g/source/createDB.py
@@ -0,0 +1,46 @@
+# /e/Fear/Univ/Big_data/Training/python/python-crawling/venv/Scripts/python
+# -*- encoding: utf-8 -*-
+
+from psycopg2 import connect, extensions
+
+def main():
+    # step 01 연결
+    conn = connecting()
+    print(conn)
+
+    createDB(conn)
+
+def connecting():
+    # DB Connect
+    conn = connect(
+        host = "localhost", # SQL 서버 주소
+        dbname = "postgres",
+        user = "postgres",
+        password = "1210",
+        port = "5432"
+    )
+
+    # print(conn)
+    return conn
+
+def createDB(conn):
+    print("createDB function.. in... ")
+    DB_NAME = "G2GtestDB"
+
+    # AutoCommit
+    autocommit = extensions.ISOLATION_LEVEL_AUTOCOMMIT
+    print("ISOLATION_LEVEL_AUTOCOMMIT:", extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    conn.set_isolation_level(autocommit)
+
+    QUERY = '''CREATE DATABASE {DBNAME}'''.format(DBNAME = DB_NAME);
+
+    cursor = conn.cursor()
+    cursor.execute(QUERY)
+    print("Database created...")
+
+    cursor.close()
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
---
+++ crawling-g2g/source/createTABLE.py
@@ -0,0 +1,60 @@
+# /e/Fear/Univ/Big_data/Training/python/python-crawling/venv/Scripts/python
+# -*- encoding: utf-8 -*-
+
+from psycopg2 import connect, extensions
+
+def main():
+    # step 01 연결
+    conn = connecting()
+    print(conn)
+
+    createTable(conn)
+
+def connecting():
+    # DB Connect
+    conn = connect(
+        host = "localhost",
+        dbname = "g2gtestdb",
+        user = "postgres",
+        password = "pwd",
+        port = "5432"
+    )
+
+    # print(conn)
+    return conn
+
+def createTable(conn):
+    print("createTable function.. in... ")
+    TABLE_NAME = "poe"
+
+    # AutoCommit
+    autocommit = extensions.ISOLATION_LEVEL_AUTOCOMMIT
+    print("ISOLATION_LEVEL_AUTOCOMMIT:", extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    conn.set_isolation_level(autocommit)
+
+    # 기존 테이블 삭제
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS TEMP")
+    print("Table DELETED....!")
+
+    # 테이블 생성 작성
+    QUERY = '''
+        CREATE TABLE {NAME}(
+            date CHAR(16),
+            seller CHAR(32),
+            server CHAR(50),
+            currency CHAR(50),
+            price CHAR(32),
+            stock CHAR(32)
+        )
+    '''.format(NAME = TABLE_NAME);
+
+    cursor.execute(QUERY)
+    print("Table created...")
+
+    cursor.close()
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
